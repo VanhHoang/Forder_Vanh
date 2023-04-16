246d8357ab4f9353379a161c0b5f1459862bd36d
VanhHoang <vietanhhoang102@gmail.com>
2023-04-16 21:33:04 +0700

    Tinh a b

--- School.java
+++ School.java
@@ -5,6 +5,10 @@ public class School {
     public static void main(String[] args) {
         System.out.println("Chao Vanh");
         System.out.println("My name is Hung");
+        int a = 10 ;
+        int b = 20 ;
+        int c = a+b;
+        System.out.println("C= "+ c);
     }
     
 }
